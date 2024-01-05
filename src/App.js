8f9b574165bb218ef488a0e88905b61a5be49dfb
Tom Burns <tomb7030@gmail.com>
2024-01-05 17:34:00 +0000

    More stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import './App.css';
 import Header from "./Components/Header.js"
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <AddItem />
+      <Header h1="Inventory Management System" p="Manage your inventory pleb"/>
+      <ButtonState/>
     </div>
   );
 }
 
-function AddItem() {
+function ButtonState() {
+const [title, setTitle] = useState("");
+const [count, setCount] = useState(0);
+
+const updateTitleClick = () => {
+  setTitle("Le' title");
+}
+
+const updateCounterClick = () => {
+  setCount(count+1);
+}
+
   return(
     <div>
-      <br/>
-      <label htmlFor="text-form">Add item: </label>
-      <input type="text" id="text-form"></input>
+      <Data title={title} count={count}/>
+      <button onClick={updateTitleClick}>Update Title</button>
+      <button onClick={updateCounterClick}>Update Counter</button>
     </div>
   )
+}
+
+function Data(props){
+return(<div>
+  <p>Title: {props.title}</p>
+  <p>Count: {props.count}</p>
+</div>)
 }
 
 export default App;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,22 @@
-function Header() {
-  const title = "Inventory Management System";
+import { PropTypes } from "prop-types";
 
+function Header(props) {
   return (
     <div>
-      <h1>{title}</h1>
-      <p>Manage your inventory please its a mess</p>
+      <h1>{props.h1}</h1>
+      <p>{props.p}</p>
     </div>
   );
+}
+
+Header.defaultProps = {
+  h1: "header",
+  p: "desc"
+}
+
+Header.propTypes = {
+  h1: PropTypes.string,
+  p: PropTypes.string
 }
 
 export default Header;
